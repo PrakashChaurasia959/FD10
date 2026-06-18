9ca54c7e4b8b0b19d27e3fa65bb53d6f51cec0c1
Prakash Chaurasia <prakashchaurasia959@gmail.com>
2026-06-18 11:04:13 +0530

    mai kuch chenge kiya hu

---
+++ Operator.js
@@ -0,0 +1,45 @@
+//Arithmetic Oper-> +,-,*,%,--,++,**/
+//+Addition
+a=9
+b=9,c=10
+console.log(a+b+c);
+// -subtraction
+a=9
+b=9
+c=20
+console.log(a-b-c);
+//multiplication
+a=9
+b=3
+console.log(a*b);
+// / division
+a=9
+b=3
+console.log(a/b);
+// % perctage
+a=9
+b=100
+console.log(a%b);
+//++incremnt
+a=9
+a++
+console.log(a);
+b=15
+//--decremnet
+b--
+console.log(b);
+c=17
+
+console.log(c);
+
+//**expontetial */
+exp=2**3
+console.log(exp);
+console.log(exp);
+
+
+
+
+
+
+
